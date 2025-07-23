8d957f53c6c8e8374b74b3edd199d6037ca752e4
MaiMinhQuan <kokobay2004@gmail.com>
2025-07-23 14:26:54 +0700

    Chapter 8

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,18 @@ Rails.application.routes.draw do
 
   #config/routes.rb
   scope "(:locale)", locale: /en|vi/ do
-    get "microposts/index"
+    root "microposts#index"
+
     get "static_pages/home"
     get "static_pages/help"
     get "static_pages/contact"
+
     get "signup", to: "users#new"
     post "signup", to: "users#create"
+
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
 
     resources :users, only: %i(show)
     resources :microposts, only: [:index]
