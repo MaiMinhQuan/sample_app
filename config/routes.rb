685197269c5b1af6de80f4915537ee6aab74c029
MaiMinhQuan <kokobay2004@gmail.com>
2025-07-23 14:26:54 +0700

    Chapter 7

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
     get "static_pages/home"
     get "static_pages/help"
     get "static_pages/contact"
+    get "signup", to: "users#new"
+    post "signup", to: "users#create"
 
+    resources :users, only: %i(show)
     resources :microposts, only: [:index]
     resources :products
   end
