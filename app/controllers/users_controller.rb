5520eabeb11280c8b833fa85d8eead248816dd30
MaiMinhQuan <kokobay2004@gmail.com>
2025-07-25 17:16:38 +0700

    Chapter_9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = I18n.t(".create_success")
       redirect_to @user, status: :see_other
     else
